73d91015534e4473b67cc4e11b3897b2d71cca54
Thiago Almeida <thiagofalmd@gmail.com>
2015-06-30 14:54:18 -0300

    Added Fragment

---
+++ app/src/main/java/com/tfalmd/theroadsofar/finale/choosewisely/FragmentTalk.java
@@ -0,0 +1,36 @@
+package com.tfalmd.theroadsofar.finale.choosewisely;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class FragmentTalk extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_talk, container, false);
+        ImageView iv = (ImageView) v.findViewById(R.id.ivFragment);
+
+        Random r = new Random();
+
+        if ((r.nextInt() % 4) == 0) {
+            iv.setImageResource(R.drawable.imbatman);
+        } else {
+            iv.setImageResource(R.drawable.right);
+        }
+
+        return v;
+    }
+}
